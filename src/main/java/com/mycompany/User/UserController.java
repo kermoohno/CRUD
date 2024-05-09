86e9c7e2d46527ab405c56865de70f7420af0db5
Kermo <kermo.ohno@voco.com>
2024-05-10 00:47:45 +0300

    Added functionality to adding new user and added success message

--- src/main/java/com/mycompany/User/UserController.java
+++ src/main/java/com/mycompany/User/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -23,5 +25,13 @@ public class UserController {
     public String showNewForm(Model model){
         model.addAttribute("user", new User());
         return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String savedUser(User user, RedirectAttributes ra) {
+        service.save(user);
+        ra.addFlashAttribute("message", "The user has been saved successfully.");
+
+        return "redirect:/users";
     }
 }
